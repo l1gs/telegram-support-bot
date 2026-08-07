6214cfd894c7212e9e746582d817ff45f0ffeada
Ali Al-Asadi <ali.h.j96.5@gmail.com>
2026-08-07 04:52:13 +0300

    Refactor message handling and add reply mapping

--- bot.py
+++ bot.py
@@ -4,35 +4,45 @@ from config import TOKEN, ADMIN_ID
 
 bot = telebot.TeleBot(TOKEN)
 
-users = {}
+reply_map = {}
 
-@bot.message_handler(func=lambda m: True)
-def all_messages(message: Message):
-    # إذا كانت الرسالة من المدير
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.reply_to(message, "أرسل رسالتك إلى الإدارة.")
+
+@bot.message_handler(func=lambda m: True, content_types=[
+    "text","photo","video","document","voice","audio","sticker"
+])
+def handler(message):
+
     if message.from_user.id == ADMIN_ID:
-        if message.reply_to_message:
-            txt = message.reply_to_message.text or ""
-            if txt.startswith("ID:"):
-                try:
-                    user_id = int(txt.split("\n")[0].replace("ID:", ""))
-                    bot.copy_message(
-                        chat_id=user_id,
-                        from_chat_id=message.chat.id,
-                        message_id=message.message_id
-                    )
-                except:
-                    pass
-        return
 
-    # إرسال رسالة المستخدم إلى المدير
-    header = f"ID:{message.from_user.id}\n"
-    if message.content_type == "text":
-        bot.send_message(ADMIN_ID, header + message.text)
-    else:
+        if not message.reply_to_message:
+            return
+
+        key = str(message.reply_to_message.message_id)
+
+        if key not in reply_map:
+            return
+
+        user_id = reply_map[key]
+
         bot.copy_message(
-            chat_id=ADMIN_ID,
+            chat_id=user_id,
             from_chat_id=message.chat.id,
             message_id=message.message_id
         )
 
+        return
+
+    sent = bot.copy_message(
+        chat_id=ADMIN_ID,
+        from_chat_id=message.chat.id,
+        message_id=message.message_id
+    )
+
+    reply_map[str(sent.message_id)] = message.chat.id
+
+print("Bot Started")
+bot.infinity_polling(skip_pending=True)
 bot.infinity_polling()
